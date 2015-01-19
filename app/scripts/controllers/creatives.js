9e927ff662601d5d97822faa4a2aeda25d4c7cbf
Rui Onodera <deraru@gmail.com>
2015-01-19 16:34:29 +0900

    refactor creative uploader

--- app/scripts/controllers/creatives.js
+++ app/scripts/controllers/creatives.js
@@ -8,44 +8,22 @@
  * Controller of the goyoukikiApp
  */
 angular.module('goyoukikiApp')
-  .controller('CreativesCtrl', function ($scope, $upload, Creative) {
+  .controller('CreativesCtrl', function ($scope, $upload, Creative, CreativeService) {
     $scope.creatives = Creative.getList().$object;
     $scope.files = '';
     $scope.$watch('files', function() {
       for (var i = 0; i < $scope.files.length; i++) {
         var file = $scope.files[i];
-        Creative.one('new').get().then(function(response) {
-          file.upload = $upload.upload({
-            url: response.uploadHostUrl,
-            method: 'POST',
-            transformRequest: function (data, headersGetter) {
-              //Headers change here
-              var headers = headersGetter();
-              delete headers.Authorization;
-              return data;
-            },
-            data: {
-              key: response.uploadPath + '${filename}',
-              AWSAccessKeyId: response.key,
-              acl: 'public-read',
-              Policy: response.policy,
-              Signature: response.signature,
-              'Content-Type': file.type === null || file.type === '' ? 'application/octet-stream' : file.type
-            },
-            file: file
-          }).success(
-            function(data, status, headers, config) {
-              Creative.post({url: response.uploadHostUrl + response.uploadPath + config.file.name}).then(
-                function(creative) {
-                  $scope.creatives.unshift(creative);
-                }
-              );
-            }).error(
-            function(data, status, headers, config) {
-              console.log('error response: ' + data + ' status: ' + status + ' headers: ' + headers + ' config: ' + config);
-            }
-          );
-        });
+        CreativeService.create(file).then(
+          function(creative) {
+            console.log('success at creative.js');
+            console.log(creative);
+            $scope.creatives.unshift(creative);
+          },
+          function(data, status, headers, config) {
+            console.log('error response: ' + data + ' status: ' + status + ' headers: ' + headers + ' config: ' + config);
+          }
+        );
       }
     });
   });
